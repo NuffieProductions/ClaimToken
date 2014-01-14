3d334ea9c6475b5c8d3a2437310dbc2a84f594e6
dznz <daniel.zollinger@crichq.com>
2014-01-14 15:50:49 +1300

    Add CodeClimate test coverage reporting
    How much coverage does the gem have? ALL THE COVERAGE.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,14 @@
+# Test coverage startup
+if ENV['CODECLIMATE_REPO_TOKEN'] # For delicious CodeClimate coverage
+  require "codeclimate-test-reporter"
+  CodeClimate::TestReporter.start
+elsif ENV['SIMPLECOV']
+  # Set the environment variable if you want to generate a detailed
+  # coverage report on your local box
+  require "simplecov"
+  SimpleCov.start
+end
+
 def stub_random_iv
   before do
     cipher = OpenSSL::Cipher::Cipher.new(ClaimToken.configuration.cipher_type)
